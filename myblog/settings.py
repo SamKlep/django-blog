9c6df60dd2c4b348de86089451830d6d1c17fa2e
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-07-31 11:37:20 -0500

    Share this post feature now sending email

--- myblog/settings.py
+++ myblog/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'samklepdev@gmail.com'
+EMAIL_HOST_PASSWORD = '&BDzEx3LyPCp8H}zeoGsMc&QFP4rqrFKhEP'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
