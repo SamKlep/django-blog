ac7ddae461907d97aab1045bd5bb0c65a4c7e26c
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-07-31 16:08:35 -0500

    comment system and tagging system now live

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,22 @@
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
-from django.views.generic import ListView
-from .models import Post
-from .forms import EmailPostForm
 from django.core.mail import send_mail
+from django.views.generic import ListView
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -22,7 +30,8 @@ def post_list(request):
     return render(request,
                  'blog/post/list.html',
                  {'page': page,
-                  'posts': posts})
+                  'posts': posts,
+                  'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -31,9 +40,31 @@ def post_detail(request, year, month, day, post):
                                    publish__year=year,
                                    publish__month=month,
                                    publish__day=day)
+
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,})
 
 
 class PostListView(ListView):
@@ -42,8 +73,9 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
+
 def post_share(request, post_id):
-    # Retreive post by id
+    # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
 
@@ -63,5 +95,5 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,
-                                                     'form': form,
-                                                     'sent': sent})
+                                                    'form': form,
+                                                    'sent': sent})
