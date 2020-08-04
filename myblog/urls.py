3c58cc556b3637370b8bf6022c8be16c51728a1f
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-08-04 08:39:58 -0500

    changed name of blog to Bloom. Added bootsrap and a navbar. Made logo a link to /blog.

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,4 +1,5 @@
-"""mysite URL Configuration
+"""myblog URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
